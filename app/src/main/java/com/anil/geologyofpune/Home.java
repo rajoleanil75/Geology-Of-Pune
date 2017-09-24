f2ad9976a625e87c943dbdcb06987a0eb7e020fa
anil <rajoleanil75@gmail.com>
2017-09-24 12:41:02 +0530

    Home Screen Menu Completed

--- app/src/main/java/com/anil/geologyofpune/Home.java
+++ app/src/main/java/com/anil/geologyofpune/Home.java
@@ -1,7 +1,11 @@
 package com.anil.geologyofpune;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 /**
@@ -9,11 +13,27 @@ import android.widget.Button;
  */
 
 public class Home extends AppCompatActivity {
+    final Context context=this;
+    Button intro,lava,area,gallery,mineral,about;
     @Override
     public void onCreate(Bundle saveInstanceState)
     {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.home);
+
+        intro= (Button) findViewById(R.id.intro);
+        lava= (Button) findViewById(R.id.lava);
+        area= (Button) findViewById(R.id.area);
+        gallery= (Button) findViewById(R.id.gallery);;
+        mineral= (Button) findViewById(R.id.minerals);
+        about= (Button) findViewById(R.id.about);
+        intro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(context,Home.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
